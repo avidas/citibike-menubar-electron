af681cd05729cda86fc71e8de781712e72f6c087
Avi Das <avi@aviadas.com>
2017-11-19 16:23:45 -0500

    Add icon

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const menubar = require('menubar')
 const gbfs = require('gbfs-client')
 const gbfsClient = new gbfs('https://gbfs.citibikenyc.com/gbfs/en/')
+const path = require('path')
 
-var mb = menubar()
+const opts = {dir: __dirname, icon: path.join(__dirname, 'images', 'Icon.png')}
+var mb = menubar(opts)
+
+var getAvailability = function () {
+  // get gbfs format citibike information
+};
 
 mb.on('ready', function ready () {
   console.log('app is ready')

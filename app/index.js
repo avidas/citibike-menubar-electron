fc6b3d010c861af6d8c1c87e6d2e695b9595d1a9
Avi Das <avi@aviadas.com>
2017-11-26 15:13:39 -0500

    Wire up index and styles and attempt to communicate via ipc

--- app/index.js
+++ app/index.js
@@ -4,10 +4,12 @@ const path = require('path')
 const opts = {dir: __dirname, icon: path.join(__dirname, '../images', 'Icon.png')}
 var mb = menubar(opts)
 
-var getAvailability = function () {
-  // get gbfs format citibike information
-};
+mb.on('ready', () => {
+  console.log('ready')
+})
 
-mb.on('ready', function ready () {
-  
+mb.on('after-create-window', () => {
+  console.log('ipc caller')
+  mb.window.loadURL(`file://${__dirname}/index.html`)
+  mb.window.webContents.send('show-stations')
 })

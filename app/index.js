ca03b03aff566a6cdc662cd158b75b5e4069f89b
Avi Das <avi@aviadas.com>
2017-11-26 13:39:22 -0500

    fix icon

--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
 const menubar = require('menubar')
-const gbfs = require('gbfs-client')
-const gbfsClient = new gbfs('https://gbfs.citibikenyc.com/gbfs/en/')
 const path = require('path')
 
-const opts = {dir: __dirname, icon: path.join(__dirname, 'images', 'Icon.png')}
+const opts = {dir: __dirname, icon: path.join(__dirname, '../images', 'Icon.png')}
 var mb = menubar(opts)
 
 var getAvailability = function () {
@@ -11,4 +9,5 @@ var getAvailability = function () {
 };
 
 mb.on('ready', function ready () {
+  
 })
--- app/renderer.js
+++ app/renderer.js
@@ -1,1 +1,3 @@
 const electron = require('electron')
+const gbfs = require('gbfs-client')
+const gbfsClient = new gbfs('https://gbfs.citibikenyc.com/gbfs/en/')

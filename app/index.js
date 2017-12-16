4d32ebe93ab606b6b54dd0b95ad44206d7707685
Avi Das <avi@aviadas.com>
2017-12-16 17:01:48 -0500

    Dont need ipc here

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const menubar = require('menubar')
 const path = require('path')
 
 const opts = {dir: __dirname, icon: path.join(__dirname, '../images', 'Icon.png')}
+
 var mb = menubar(opts)
 
 mb.on('ready', () => {
@@ -9,7 +10,5 @@ mb.on('ready', () => {
 })
 
 mb.on('after-create-window', () => {
-  console.log('ipc caller')
   mb.window.loadURL(`file://${__dirname}/index.html`)
-  mb.window.webContents.send('show-stations')
 })

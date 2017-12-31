7a36816765287fff3512850969dc58b69056b96c
Avi Das <avi@aviadas.com>
2017-12-31 18:49:31 -0500

    Most of the current logic for nearest citibikes. Not my greatest but this works for now

--- app/renderer.js
+++ app/renderer.js
@@ -8,111 +8,106 @@ const gbfsClient = new gbfs('https://gbfs.citibikenyc.com/gbfs/en/')
 const $stationsList = $('.station-list')
 const createStationElement = require('./support/create-station-element')
 
-const $citbikeButton = $('#load-citibike-data')
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+const GEOLOCATION_URI = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + GOOGLE_API_KEY
 
-ipc.on('show-stations', (event) => {
-  addStationtoList()
-})
+showStationsForCurrentLocation = () => {
+    $.post(GEOLOCATION_URI)
+        .done((data) => {
+            const position = {
+                lat: data.location.lat,
+                lon: data.location.lng
+            }
+            addStationtoList(position)
+        })
+}
 
-addStationtoList = () => {
-  // get gbfs format citibike information
-  gbfsClient.stationInfo()
-    .then(stations => {
-      return getNearestNStations(stations, 5)
-    })
-    .then((nearestStations) => {
-      showNearestNStations(nearestStations)
-    })
+addStationtoList = (position) => {
+    // get gbfs format citibike information
+    gbfsClient.stationInfo()
+        .then(stations => {
+            return getNearestNStations(position, stations, 5)
+        })
+        .then((nearestStations) => {
+            showNearestNStations(nearestStations)
+        })
 }
 
 notifyAvailabilty = () => {
-  new Notification('Citibike Status', {
-    body: "Lots of bikes currently available"
-  })
+    new Notification('Citibike Status', {
+        body: "Lots of bikes currently available"
+    })
 }
 
 showNearestNStations = (nearestStations) => {
-  nearestStationsWithAvailability = []
-  nearestStations.forEach((station) => {
+    nearestStationsWithAvailability = []
+    nearestStations.forEach((station) => {
 
-    gbfsClient.stationStatus(station.station_id)
-      .then( (status) => {
-        station.num_bikes_available = status.num_bikes_available
-        station.num_docks_available = status.num_docks_available
+        gbfsClient.stationStatus(station.station_id)
+            .then((status) => {
+                station.num_bikes_available = status.num_bikes_available
+                station.num_docks_available = status.num_docks_available
 
-        nearestStationsWithAvailability.push(station)
-        const $station = createStationElement(station.name, status.num_bikes_available, status.num_docks_available, station.distance)
-        $stationsList.append($station)
-      })
-  })
-  return nearestStationsWithAvailability
-}
-
-getNearestNStations = (stations, n) => {
-  const stationCount = stations.length
-
-  const position = {
-    lat: 40.7055,
-    lon: -74.0088
-  }
-
-  var closest_loc
-  var closest_dist = 23423432
-
-  var stationWithDist = []
-
-  stations.forEach((station) => {
-    station_distance = distance(station, position)
-
-    stationWithDist.push({
-      name: station.name,
-      distance: station_distance,
-      lat: station.lat,
-      lon: station.lon,
-      station_id: station.station_id
+                nearestStationsWithAvailability.push(station)
+                const $station = createStationElement(station.name, status.num_bikes_available, status.num_docks_available, station.distance)
+                $stationsList.append($station)
+            })
     })
-  })
-
-  stationCompare = (a, b) => {
-    return a.distance - b.distance
-  }
-
-  stationsWithDistSort = stationWithDist.sort(stationCompare)
-  const nearestN = stationsWithDistSort.slice(0, n)
-
-  return nearestN
+    return nearestStationsWithAvailability
 }
 
-function distance(position1,position2){
-    var lat1=position1.lat;
-    var lat2=position2.lat;
-    var lon1=position1.lon;
-    var lon2=position2.lon;
+getNearestNStations = (position, stations, n) => {
+    const stationCount = stations.length
+
+    var closest_loc
+    var closest_dist = 23423432
+
+    var stationWithDist = []
+
+    stations.forEach((station) => {
+        station_distance = distance(station, position)
+
+        stationWithDist.push({
+            name: station.name,
+            distance: station_distance,
+            lat: station.lat,
+            lon: station.lon,
+            station_id: station.station_id
+        })
+    })
+
+    stationCompare = (a, b) => {
+        return a.distance - b.distance
+    }
+
+    stationsWithDistSort = stationWithDist.sort(stationCompare)
+    const nearestN = stationsWithDistSort.slice(0, n)
+
+    return nearestN
+}
+
+function distance(position1, position2) {
+    var lat1 = position1.lat;
+    var lat2 = position2.lat;
+    var lon1 = position1.lon;
+    var lon2 = position2.lon;
     var R = 6371000; // metres
     var φ1 = toRadians(lat1);
     var φ2 = toRadians(lat2);
-    var Δφ = toRadians((lat2-lat1));
-    var Δλ = toRadians((lon2-lon1));
+    var Δφ = toRadians((lat2 - lat1));
+    var Δλ = toRadians((lon2 - lon1));
 
-    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+    var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
         Math.cos(φ1) * Math.cos(φ2) *
-        Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     var d = R * c;
     return d;
 }
 
 function toRadians(x) {
-   return x * Math.PI / 180;
+    return x * Math.PI / 180;
 }
 
-$citbikeButton.on('click', addStationtoList)
-
-navigator.geolocation.getCurrentPosition(location => {
-  console.log('getCurrentPosition')
-  console.log(process.env.GOOGLE_API_KEY)
-  console.log(location)
-})
-
-addStationtoList()
+showStationsForCurrentLocation()

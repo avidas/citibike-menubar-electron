6077a2947723f55b0c39b45dc382b28ac165e412
Avi Das <avi@aviadas.com>
2017-12-31 18:45:44 -0500

    This formatter seems aggressive

--- app/index.js
+++ app/index.js
@@ -1,14 +1,14 @@
 const menubar = require('menubar')
 const path = require('path')
 
-const opts = {dir: __dirname, icon: path.join(__dirname, '../images', 'Icon.png')}
+const opts = { dir: __dirname, icon: path.join(__dirname, '../images', 'Icon.png') }
 
 var mb = menubar(opts)
 
 mb.on('ready', () => {
-  console.log('ready')
+    console.log('ready')
 })
 
 mb.on('after-create-window', () => {
-  mb.window.loadURL(`file://${__dirname}/index.html`)
+    mb.window.loadURL(`file://${__dirname}/index.html`)
 })
